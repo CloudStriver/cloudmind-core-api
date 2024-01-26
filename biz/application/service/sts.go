5994ca94e8234d701c9e7d6034c8d9f4fdf6c58a
Lansong <62054128+Lansongxx@users.noreply.github.com>
2024-01-26 16:45:17 +0800

    fix: 修改预签名api (#14)

--- biz/application/service/sts.go
+++ biz/application/service/sts.go
@@ -6,7 +6,9 @@ import (
 	"github.com/CloudStriver/cloudmind-core-api/biz/adaptor"
 	"github.com/CloudStriver/cloudmind-core-api/biz/application/dto/cloudmind/core_api"
 	"github.com/CloudStriver/cloudmind-core-api/biz/infrastructure/consts"
+	"github.com/CloudStriver/cloudmind-core-api/biz/infrastructure/rpc/cloudmind_content"
 	"github.com/CloudStriver/cloudmind-core-api/biz/infrastructure/rpc/cloudmind_sts"
+	"github.com/CloudStriver/service-idl-gen-go/kitex_gen/cloudmind/content"
 	"github.com/CloudStriver/service-idl-gen-go/kitex_gen/cloudmind/sts"
 
 	"github.com/google/wire"
@@ -19,7 +21,8 @@ type IStsService interface {
 }
 
 type StsService struct {
-	PlatformSts cloudmind_sts.ICloudMindSts
+	PlatformSts      cloudmind_sts.ICloudMindSts
+	CloudMindContent cloudmind_content.ICloudMindContent
 }
 
 var StsServiceSet = wire.NewSet(
@@ -35,26 +38,36 @@ func (s *StsService) ApplySignedUrl(ctx context.Context, req *core_api.ApplySign
 	resp := new(core_api.ApplySignedUrlResp)
 	userId := user.GetUserId()
 	fmt.Println(userId)
-	//if req.File {
-	//	 TODO：扣取用户流量
-	//}
-
+	if req.File {
+		//	 TODO：扣取用户流量
+	}
 	// TODO：判断是否已经上传过
+	getFileIsExist, err := s.CloudMindContent.GetFileIsExist(ctx, &content.GetFileIsExistReq{
+		Md5: req.Md5,
+	})
+	if err != nil {
+		return resp, err
+	}
 
-	data, err := s.PlatformSts.GenCosSts(ctx, &sts.GenCosStsReq{Path: "users/*"})
+	if getFileIsExist.Ok {
+		resp.Url = "exist"
+		return resp, nil
+	}
+
+	genCosStsResp, err := s.PlatformSts.GenCosSts(ctx, &sts.GenCosStsReq{Path: "users/*"})
 	if err != nil {
 		return nil, err
 	}
-	resp.SessionToken = data.SessionToken
-	data2, err := s.PlatformSts.GenSignedUrl(ctx, &sts.GenSignedUrlReq{
-		SecretId:  data.SecretId,
-		SecretKey: data.SecretKey,
+	resp.SessionToken = genCosStsResp.SessionToken
+	genSignedUrlResp, err := s.PlatformSts.GenSignedUrl(ctx, &sts.GenSignedUrlReq{
+		SecretId:  genCosStsResp.SecretId,
+		SecretKey: genCosStsResp.SecretKey,
 		Method:    http.MethodPut,
 		Path:      "users/" + req.Md5 + req.Suffix,
 	})
 	if err != nil {
 		return resp, err
 	}
-	resp.Url = data2.SignedUrl
+	resp.Url = genSignedUrlResp.SignedUrl
 	return resp, nil
 }
