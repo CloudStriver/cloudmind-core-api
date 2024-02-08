f305bde953e77b71ce9060f95c264e33b2067c7d
Lansong <62054128+Lansongxx@users.noreply.github.com>
2024-02-08 15:49:18 +0800

    fix: 修复缺少的参数 (#38)

--- biz/application/service/post.go
+++ biz/application/service/post.go
@@ -203,7 +203,23 @@ func (s *PostService) GetOwnPosts(ctx context.Context, req *core_api.GetOwnPosts
 				Url:    item.Url,
 				Tags:   item.Tags,
 			}
-			s.PostDomainService.LoadLikeCount(ctx, &resp.Posts[i].LikeCount, item.PostId) // 点赞量
+			author := &core_api.User{}
+			if err = mr.Finish(func() error {
+				s.PostDomainService.LoadLikeCount(ctx, &resp.Posts[i].LikeCount, item.PostId) // 点赞量
+				return nil
+			}, func() error {
+				// 加载评论量
+				return nil
+			}, func() error {
+				s.PostDomainService.LoadLiked(ctx, &resp.Posts[i].Liked, userData.UserId, item.PostId)
+				return nil
+			}, func() error {
+				s.PostDomainService.LoadAuthor(ctx, author, item.UserId)
+				resp.Posts[i].UserName = author.Name
+				return nil
+			}); err != nil {
+				return err
+			}
 			return nil
 		}
 	})...); err != nil {
@@ -262,6 +278,7 @@ func (s *PostService) GetOtherPost(ctx context.Context, req *core_api.GetOtherPo
 
 func (s *PostService) GetOtherPosts(ctx context.Context, req *core_api.GetOtherPostsReq) (resp *core_api.GetOtherPostsResp, err error) {
 	resp = new(core_api.GetOtherPostsResp)
+	userData := adaptor.ExtractUserMeta(ctx)
 	var (
 		getPostsResp  *content.GetPostsResp
 		searchOptions *content.SearchOptions
@@ -313,7 +330,25 @@ func (s *PostService) GetOtherPosts(ctx context.Context, req *core_api.GetOtherP
 				Url:    item.Url,
 				Tags:   item.Tags,
 			}
-			s.PostDomainService.LoadLikeCount(ctx, &resp.Posts[i].LikeCount, item.PostId) // 点赞量
+			author := &core_api.User{}
+			if err = mr.Finish(func() error {
+				s.PostDomainService.LoadLikeCount(ctx, &resp.Posts[i].LikeCount, item.PostId) // 点赞量
+				return nil
+			}, func() error {
+				// 加载评论量
+				return nil
+			}, func() error {
+				if userData.GetUserId() != "" {
+					s.PostDomainService.LoadLiked(ctx, &resp.Posts[i].Liked, userData.GetUserId(), item.PostId)
+				}
+				return nil
+			}, func() error {
+				s.PostDomainService.LoadAuthor(ctx, author, item.UserId)
+				resp.Posts[i].UserName = author.Name
+				return nil
+			}); err != nil {
+				return err
+			}
 			return nil
 		}
 	})...); err != nil {
